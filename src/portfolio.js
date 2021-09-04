6531e3354770a4c46428617af1ad4b283fb46a53
Suraj Vatsya <surajkumar742494@gmail.com>
2021-09-05 00:59:47 +0530

    portfolio added

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,26 +11,26 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Suraj Kumar",
+  title: "Hi all, I'm Suraj",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate competitive programmer and Full Stack Web Developer 🚀 having an experience of building Web  applications with JavaScript / Reactjs / Nodejs / Express js and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing",
+    "https://drive.google.com/file/d/1QP0Hyjp-wg9lIuH3LcxphAwBp4bpQRJq/view?usp=sharing",
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/Surajvatsya",
+  linkedin: "https://www.linkedin.com/in/suraj-kumar-49b466191/",
+  gmail: "surajkumar742494@gmail.com",
+  // gitlab: "https://gitlab.com/saadpasta",
+  facebook: "https://www.facebook.com/profile.php?id=100010302857590",
+  // medium: "https://medium.com/@saadpasta",
+  stackoverflow: "https://stackoverflow.com/users/14767181/suraj-kumar-030",
   // Instagram and Twitter are also supported in the links!
   display: true // Set true to display this section, defaults to false
 };
@@ -62,10 +62,7 @@ https://fontawesome.com/icons?d=gallery */
       skillName: "css3",
       fontAwesomeClassname: "fab fa-css3-alt"
     },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
+    
     {
       skillName: "JavaScript",
       fontAwesomeClassname: "fab fa-js"
@@ -78,10 +75,7 @@ https://fontawesome.com/icons?d=gallery */
       skillName: "nodejs",
       fontAwesomeClassname: "fab fa-node"
     },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
+    
     {
       skillName: "npm",
       fontAwesomeClassname: "fab fa-npm"
@@ -102,10 +96,7 @@ https://fontawesome.com/icons?d=gallery */
       skillName: "python",
       fontAwesomeClassname: "fab fa-python"
     },
-    {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
-    }
+    
   ],
   display: true // Set false to hide this section, defaults to true
 };
@@ -116,23 +107,29 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
+      schoolName: "Techno Main Salt Lake",
+      logo: require("./assets/images/collegePic.jpg"),
+      subHeader: "Bechlors of Technology in Computer Science",
+      duration: "July 2019 - July 2023",
+      desc: "Participated in Tech Fest INTRA 2019 ,Youth Parliament 2019, White Collar under E-Weekend 3.0 organised by JADHAVPUR UNIVERSITY West Bengal",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        "Winner at FIVE FEET Drop organised by Eclectica TMSL",
+        "Participate in Toys and Game Virtual Hackathon organised by TECHNO INDIA UNIVERSITY kolkata",
+        "Successfully completed project in Vihan 4.0 organised by IEEE COMPUTER SOCIETY DTU",
+        "Participated in largest student-level entrepreneurial summit in India Entrepreneurship Awareness Drive organised by IIT KHARAGPUR"
       ]
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "Saraswati Vidya Mandir ",
+      logo: require("./assets/images/Schoolpic.jpg"),
+      subHeader: "Senior High School",
+      duration: "April 2006 - April 2016",
+      desc: "Ranked 10 CGPA in 10th class from CBSE Board.",
+      descBullets: ["Participated in N.S.A.T 2015 and secured 95% got 1st rank in this examination.",
+    "Grab 15th position in TALASH under THE QUALIFIER in 2016 and got 50% scholarship.",
+    "Participated in PROVINCIAL ENGLISH QUIZ COMPETITION 2010 and got CERTIFICATE OF FELICATION by LOK SHIKSHA SAMITI BIHAR sponsored by SARASWATI VIDYA MANDIR SITAMARHI",
+    "Project Leader in Science Fair in 2013 under Vidya Bharti and got 2nd rank"
+    ]
     }
   ]
 };
@@ -144,15 +141,15 @@ const techStack = {
   experience: [
     {
       Stack: "Frontend/Design", //Insert stack or technology you have experience in
-      progressPercentage: "90%" //Insert relative proficiency in percentage
+      progressPercentage: "80%" //Insert relative proficiency in percentage
     },
     {
       Stack: "Backend",
-      progressPercentage: "70%"
+      progressPercentage: "90%"
     },
     {
       Stack: "Programming",
-      progressPercentage: "60%"
+      progressPercentage: "80%"
     }
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
@@ -161,7 +158,7 @@ const techStack = {
 // Work experience section
 
 const workExperiences = {
-  display: true, //Set it to true to show workExperiences Section
+  display: false, //Set it to true to show workExperiences Section
   experience: [
     {
       role: "Software Engineer",
@@ -203,28 +200,28 @@ const openSource = {
 
 const bigProjects = {
   title: "Big Projects",
-  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  subtitle: "SOME PROGRAMS AND COMPETITIONS THAT I HAVE PARTICIPATED IN TO BUILD THESE PROJECTS",
   projects: [
     {
-      image: require("./assets/images/saayaHealthLogo.webp"),
-      projectName: "Saayahealth",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/todo.jpg"),
+      projectName: "Custom To-Do-List",
+      projectDesc: "Custom list of tasks you need to complete, or things that you want to do",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://saayahealth.com/"
+          url: "https://infinite-peak-71433.herokuapp.com/"
         }
         //  you can add extra buttons here.
       ]
     },
     {
-      image: require("./assets/images/nextuLogo.webp"),
-      projectName: "Nextu",
-      projectDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      image: require("./assets/images/blog2.jpg"),
+      projectName: "Blog Website",
+      projectDesc: "a beautiful personal blog website, Now when you head over to the home page, this is going to have all of your blog posts on here listed",
       footerLink: [
         {
           name: "Visit Website",
-          url: "http://nextu.se/"
+          url: "https://pure-shelf-88929.herokuapp.com/"
         }
       ]
     }
@@ -242,47 +239,38 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "Vihaan 4.0 Hackathon  IEEE COMPUTER SOCIETY DTU",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
+        "grateful to IEEE COMPUTER SOCIETY for having us precious opportunities. Hackathon was full enlightened by gems, it was a great experience to participate in VIHAAN 4.0",
+      image: require("./assets/images/Teciss.jpg"),
       footerLink: [
         {
           name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-        },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          url: "https://www.linkedin.com/feed/update/urn:li:activity:6795596519564300288/"
         }
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "Toys and  Game Virtual hackathon 2021",
       subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
+        "Participated in toys & virtual hackathon created a Javascript Car Racing Game.",
+      image: require("./assets/images/racing.jpg"),
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+          name: "Play Game",
+          url: "https://car-racing-app-surajvatsya.herokuapp.com/"
         }
       ]
     },
 
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
+      title: "Drum kit",
+      subtitle: "A Drum Kit for versatile pleasant sounds",
+      image: require("./assets/images/drumKit.jpg"),
       footerLink: [
-        {name: "Certification", url: ""},
         {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
+          name: "Play Drum Kit",
+          url: "https://surajvatsya.github.io/Drum-Kit/"
         }
       ]
     }
@@ -295,20 +283,20 @@ const achievementSection = {
 const blogSection = {
   title: "Blogs",
   subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+    "With Love for narration , I love to write and think creative.",
 
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://www.blogger.com/blog/post/edit/9084113457878913388/423896853357105823",
+      title: "A short Film :- ROTATION",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+        " Love Story during college life"
     },
     {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
+      url: "https://www.blogger.com/blog/post/edit/9084113457878913388/1718775683052218906",
+      title: "EXTRACTION",
       description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+        "This movie runs around two big drug dealer.First one is from  India and second  is present in Bangladesh. The famous Indian drug dealer who is in jail right now, has a kid OVI under guidance of special agent SAJU"
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -319,15 +307,15 @@ const blogSection = {
 const talkSection = {
   title: "TALKS",
   subtitle: emoji(
-    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+    "I LOVE TO SHARE MY LIMITED  KNOWLEDGE AND GET A SPEAKER BADGE 😅"
   ),
 
   talks: [
     {
-      title: "Build Actions For Google Assistant",
-      subtitle: "Codelab at GDG DevFest Karachi 2019",
-      slides_url: "https://bit.ly/saadpasta-slides",
-      event_url: "https://www.facebook.com/events/2339906106275053/"
+      title: "First Day Of My college",
+      subtitle: "Five Feet Drop organised by literacy club of TMSL, ECLECTICA",
+      slides_url: "https://www.facebook.com/watch/live/?v=2975091382747522&ref=search",
+      event_url: "https://www.instagram.com/p/B9ZT-cfpHuT4KYSiC1DolyhWYmRleZDy12qA-w0/"
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -343,21 +331,21 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-3243454077",
-  email_address: "saadpasta70@gmail.com"
+  number: "+917424948001",
+  email_address: "surajkumar742494@gmail.com"
 };
 
 // Twitter Section
 
 const twitterDetails = {
-  userName: "twitter", //Replace "twitter" with your twitter username without @
+  userName: "SurajVatsya", //Replace "twitter" with your twitter username without @
   display: true // Set true to display this section, defaults to false
 };
 
